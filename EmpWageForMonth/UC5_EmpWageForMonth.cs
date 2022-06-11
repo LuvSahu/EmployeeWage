560258beaa6142c7dcc4391c541f4b1805fb3aae
Luv sahu <lksahu404@gmail.com>
2022-06-11 17:50:31 +0530

    This code for Employee Wage for a month

---
+++ EmpWageForMonth/UC5_EmpWageForMonth.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    internal class UC5_EmpWageForMonth
+    {
+        public const int IsAbsnt = 0;
+        public const int IsPresent = 1;
+        public int Wage_Per_Hours = 20;
+        public int FullTime_Daily_Hours = 8;
+        public int DailyWage = 0;
+        public float EmpDailyWage = 0;
+        public const int IsPartTime = 2;
+        public int PartTime_Day_Hours = 4;
+        public float EmpMonthlyWage = 0;
+        public const int Max_WorkingDay = 20;
+        public int IsEmployeePresent()
+        {
+            return new Random().Next(0, 3);
+        }
+        public void CalculateWage()
+        {
+            int DayNumber = 1;
+            int EmpWorkingHrs = 0;
+            while (DayNumber <= Max_WorkingDay)
+            {
+                switch (IsEmployeePresent())
+                {
+                    case IsAbsnt:
+                        Console.WriteLine("Employee is Not Present");
+                        break;
+                    case IsPresent:
+                        Console.WriteLine("Employee is Present");
+                        EmpWorkingHrs = FullTime_Daily_Hours;
+                        break;
+                    case IsPartTime:
+                        EmpWorkingHrs = PartTime_Day_Hours;
+                        break;
+                }
+                EmpDailyWage = EmpWorkingHrs * Wage_Per_Hours;
+                EmpMonthlyWage += EmpDailyWage;
+                DayNumber++;
+            }
+            Console.WriteLine(EmpDailyWage + "Monthly Wage " + EmpMonthlyWage);
+
+        }
+    }
+}
