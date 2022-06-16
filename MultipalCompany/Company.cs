5f01aca74c1f65b73111da1857fedf823bd98fef
Luv sahu <lksahu404@gmail.com>
2022-06-16 11:06:05 +0530

    The code for Multipal Company

---
+++ MultipalCompany/Company.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageMultipleCmpnyWithDict
+{
+    internal class Company
+    {
+        public float EmpWagePerHourse = 20;
+        public int FullTime_Daily_Hours = 8;
+        public int PartTime_Day_Hours = 4;
+        public int Max_WorkingHrs = 20;
+        public int Max_WorkingDay = 100;
+        public String CompanyName;
+
+        public Company(String CompanyName, float EmpWagePerHourse, int FullTime_Daily_Hours, int PartTime_Day_Hours, int Max_WorkingHrs, int Max_WorkingDay)
+        {
+            this.CompanyName = CompanyName;
+            this.EmpWagePerHourse = EmpWagePerHourse;
+            this.FullTime_Daily_Hours = FullTime_Daily_Hours;
+            this.PartTime_Day_Hours = PartTime_Day_Hours;
+            this.Max_WorkingHrs = Max_WorkingHrs;
+            this.Max_WorkingDay = Max_WorkingDay;
+        }
+    }
+}
+
---
+++ MultipalCompany/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace EmpWageMultipleCmpnyWithDict
+{
+
+    class program
+    {
+
+        public static void Main(String[] args)
+        {
+            EmpWage Empwage = new EmpWage();
+            Empwage.AddCompany("Dell", 23, 22, 11, 44, 33);
+            Empwage.AddCompany("Tcs", 3, 2, 11, 4, 3);
+            Empwage.CalculateWage("Tcs");
+            Empwage.CalculateWage("Dell");
+        }
+    }
+}
