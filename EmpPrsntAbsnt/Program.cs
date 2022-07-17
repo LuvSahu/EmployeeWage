394cae87b4370df9d13b0bab11d5aa2d10da785b
LuvSahu <104768173+LuvSahu@users.noreply.github.com>
2022-07-17 16:33:19 +0530

    Update Program.cs

--- EmpPrsntAbsnt/Program.cs
+++ EmpPrsntAbsnt/Program.cs
@@ -4,11 +4,11 @@
     {
         public static void Main(String[]args)
         {
-            //UC1_Prsnt present = new UC1_Prsnt();
-            //present.CalculatePresent();
+            UC1_Prsnt present = new UC1_Prsnt();
+            present.CalculatePresent();
 
-            UC2_dailywage dlywage = new UC2_dailywage();
-            dlywage.CalculateWage();
+            //UC2_dailywage dlywage = new UC2_dailywage();
+            //dlywage.CalculateWage();
         }
     }
 }
